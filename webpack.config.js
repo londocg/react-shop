5edf47b7179015e214ed97610301b554bf8f2d90
Leonardo Cabezas Grajales <numeraljoker@gmail.com>
2022-10-30 22:40:12 -0500

    se incorporan assets para el proyect

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,10 @@ module.exports = {
                     "css-loader",
                     "sass-loader",
                 ],
+            },
+            {
+                test: /\.(png|svg|jpg|git)$/,
+                type: 'asset'
             }
         ]
     },
